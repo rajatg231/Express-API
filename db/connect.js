cc79c03b89d090bcb3291ede29568ffcf342d77d
Rajat <rajatg231@users.noreply.github.com>
2023-02-25 20:46:45 +0530

    Task Manager API using Express and MongoDB

---
+++ db/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    authSource: "admin",
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+}
+module.exports = connectDB
